4ea24f46dbb81657fbd0d024ce1593b38885d1dd
alisoncleonard <alisoncleonard@gmail.com>
2019-09-25 21:09:37 -0600

    started unit test file for get_data.py

---
+++ test_get_data.py
@@ -0,0 +1,32 @@
+"""
+Unit tests for get_data.py
+"""
+
+import unittest
+import get_data
+
+class TestGetData(unittest.TestCase):
+    #def setUp(self):
+    #    self.test_stdin_data = input('type tab separated data here: ')
+
+
+    #def test_stdin_creation(self):
+    #    get_data.sys.stdin = lambda _: '3 4 5 ; 6 7 8 ; 9 10 11'
+    #    m = get_data.read_stdin_col(1)
+    #    print(m)
+    #    self.assertEqual(m, ['4', '7', '10'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
