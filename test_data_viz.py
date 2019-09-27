1a908411c7f7abfc0d5085c6ff801c9180156697
alisoncleonard <alisoncleonard@gmail.com>
2019-09-26 23:29:05 -0600

    removed broken filename test

--- test_data_viz.py
+++ test_data_viz.py
@@ -6,14 +6,6 @@ import data_viz as dv
 
 class TestMathLib(unittest.TestCase):
 
-    def test_boxplot_file_in_use(self):
-        # requires test1.png file downloaded to directory
-        L = [4, 6, 5, 9, 10, 3, 9]
-        with self.assertRaises(Exception) as ex:
-            dv.boxplot(L, 'test1.png')
-        message = 'Output file already exists. Choose a new name'
-        self.assertEqual(str(ex.exception), message)
-
     def test_boxplot_out_file_type(self):
         L = [4, 6, 5, 9, 10, 3, 9]
         with self.assertRaises(ValueError) as ex:
@@ -21,27 +13,11 @@ class TestMathLib(unittest.TestCase):
         message = 'Can not support file extension. Try .png instead'
         self.assertEqual(str(ex.exception), message)
 
-    def test_histogram_file_in_use(self):
-        # requires test1.png file downloaded to directory
-        L = [4, 6, 5, 9, 10, 3, 9]
-        with self.assertRaises(Exception) as ex:
-            dv.histogram(L, 'test1.png')
-        message = 'Output file already exists. Choose a new name'
-        self.assertEqual(str(ex.exception), message)
-
     def test_histogram_out_file_type(self):
         L = [4, 6, 5, 9, 10, 3, 9]
         with self.assertRaises(ValueError) as ex:
             dv.histogram(L, 'test.jpeg')
         message = 'Can not support file extension. Try .png instead'
-        self.assertEqual(str(ex.exception), message)
-
-    def test_combo_file_in_use(self):
-        # requires test1.png file downloaded to directory
-        L = [4, 6, 5, 9, 10, 3, 9]
-        with self.assertRaises(Exception) as ex:
-            dv.combo(L, 'test1.png')
-        message = 'Output file already exists. Choose a new name'
         self.assertEqual(str(ex.exception), message)
 
     def test_combo_out_file_type(self):
